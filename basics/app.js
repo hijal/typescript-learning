67d89c551398d60eb12cc41114a25c75a91c0deb
imh1j4l <hijal@shuttlebd.com>
2022-01-03 21:36:59 +0600

    typescript object data type

--- basics/app.js
+++ basics/app.js
@@ -1,18 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-// base types of typescript.
-var number1;
-number1 = 5; // assign a number
-//number1 = '5'; // error assign a string
-var number2 = 2.8;
-var printResult = true;
-var phrase = 'Result is: ';
-// phrase = 0;  // error
-add(number1, number2, printResult, phrase);
+var person = {
+    name: 'hijal',
+    age: 27
+};
+console.log(person);
--- basics/app.ts
+++ basics/app.ts
@@ -1,21 +1,8 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-	let result = n1 + n2;
-
-	if (showResult) {
-		console.log(phrase + result);
-	} else {
-		return result;
-	}
-}
-
-// base types of typescript.
-let number1: number;
-number1 = 5; // assign a number
-//number1 = '5'; // error assign a string
-
-let number2 = 2.8;
-let printResult = true;
-let phrase = 'Result is: ';
-// phrase = 0;  // error
-
-add(number1, number2, printResult, phrase);
+const person: {
+	name: string;
+	age: number;
+} = {
+	name: 'hijal',
+	age: 27,
+};
+console.log(person);
