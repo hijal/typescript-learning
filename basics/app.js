bb8c2ab4af2607d91424f761a0331a361ba6af02
imh1j4l <hijal@shuttlebd.com>
2022-01-04 17:02:09 +0600

    literal with enum type in typescript

--- basics/app.js
+++ basics/app.js
@@ -1,10 +1,15 @@
-function combine(n1, n2, resultConversion) {
+var TypeConversion;
+(function (TypeConversion) {
+    TypeConversion[TypeConversion["AS-NUMBER"] = 0] = "AS-NUMBER";
+    TypeConversion[TypeConversion["AS-TEXT"] = 1] = "AS-TEXT";
+})(TypeConversion || (TypeConversion = {}));
+function combine(n1, n2, conversion) {
     if ((typeof n1 === 'number' && typeof n2 === 'number') ||
-        resultConversion === 'as-number') {
+        conversion === TypeConversion['AS-NUMBER']) {
         return +n1 + +n2;
     }
-    return n1.toString() + ' ' + n2.toString();
+    return n1.toString().toUpperCase() + ' ' + n2.toString().toUpperCase();
 }
-console.log(combine(10, 20, 'as-number'));
-console.log(combine('10', '20', 'as-number'));
-console.log(combine('hijikesh', 'hijal', 'as-text'));
+console.log(combine(10, 10, TypeConversion['AS-NUMBER']));
+console.log(combine('10', '10', TypeConversion['AS-NUMBER']));
+console.log(combine('hijikesh', 'hijal', TypeConversion['AS-TEXT']));
