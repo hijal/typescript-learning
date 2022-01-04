eb201530389ebeff9b4430280f9c206fa07bb6f0
imh1j4l <hijal@shuttlebd.com>
2022-01-04 16:40:28 +0600

    union type in typescript

--- basics/app.js
+++ basics/app.js
@@ -1,20 +1,8 @@
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var Person = {
-    name: 'hijal',
-    age: 27,
-    hobbies: ['cooking', 'sports'],
-    role: Role.ADMIN
-};
-console.log(Person.name);
-for (var _i = 0, _a = Person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(n1, n2) {
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        return n1 + n2;
+    }
+    return n1.toString() + ' ' + n2.toString();
 }
-if (Person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+console.log(combine(10, 20));
+console.log(combine('hijikesh', 'hijal'));
