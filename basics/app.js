05c4ce7c4048e3a3d32e3af30140ec97648ba83f
imh1j4l <hijal@shuttlebd.com>
2022-01-05 21:25:44 +0600

    function and callback type in ts

--- basics/app.js
+++ basics/app.js
@@ -1,28 +1,11 @@
-function add(a, b) {
-    return a + b;
-}
-function display(result) {
-    console.log(result);
-}
-display(add(1, 2));
-var combines;
-combines = add;
-// combines = display;
-// combines = 5;
-console.log(combines(10, 10));
-// person object js
-function findAll(name, age) {
-    return {
+function functionAndCallback(name, age, cb) {
+    var result = {
         name: name,
         age: age
     };
+    cb(result);
 }
-function response(obj) {
+functionAndCallback('hijal', 27, function (obj) {
+    console.log('callback');
     console.log(obj);
-}
-var showResponse;
-console.log('------------------------------');
-response(findAll('John', 30));
-console.log('------------------------------');
-showResponse = findAll;
-console.log(showResponse('John', 30));
+});
