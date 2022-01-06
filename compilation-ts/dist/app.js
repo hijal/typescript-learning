4c94421fa548e3d9aff5d2d172e2658f7eefe6be
imh1j4l <hijal@shuttlebd.com>
2022-01-06 12:30:50 +0600

    default parameter in ts

--- compilation-ts/dist/app.js
+++ compilation-ts/dist/app.js
@@ -1,9 +1,20 @@
 "use strict";
-var add = function (a, b) { return a + b; };
-var display = function (result) { return console.log(result); };
-display(add(1, 2));
-var button = document.querySelector('button');
-button &&
-    button.addEventListener('click', function (event) {
-        console.log(event);
-    });
+var discount = function (price, rate) {
+    if (rate === void 0) { rate = 0.1; }
+    return price * (1 - rate);
+};
+var getDay = function (month, year) {
+    if (year === void 0) { year = new Date().getFullYear(); }
+    var days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (month === 2) {
+        // leap year condition
+        if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+            return 29;
+        }
+        return 28;
+    }
+    return days[month - 1];
+};
+var display = function (result, message) { return console.log(message + ': ', result); };
+display(discount(10), 'Discount');
+display(getDay(2), "Days");
--- compilation-ts/src/app.ts
+++ compilation-ts/src/app.ts
@@ -1,12 +1,29 @@
-const add: (a: number, b: number) => number = (a: number, b: number) => a + b;
+const discount: (price: number, rate?: number) => number = (
+	price: number,
+	rate: number = 0.1
+) => {
+	return price * (1 - rate);
+};
 
-const display: (a: string | number) => void = (result) => console.log(result);
+const getDay: (month: number, year?: number) => number = (
+	month: number,
+	year: number = new Date().getFullYear()
+) => {
+	const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	if (month === 2) {
+		// leap year condition
+		if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+			return 29;
+		}
+		return 28;
+	}
+	return days[month - 1];
+};
 
-display(add(1, 2));
+const display: (a: string | number, message: string) => void = (
+	result,
+	message
+) => console.log(message + ': ', result);
 
-const button = document.querySelector('button');
-
-button &&
-	button.addEventListener('click', (event) => {
-		console.log(event);
-	});
+display(discount(10), 'Discount');
+display(getDay(2), `Days`);
