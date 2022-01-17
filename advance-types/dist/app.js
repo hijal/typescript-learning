dba445cef2e153bb0df4ed03754354e244b5e94b
imh1j4l <hijal@shuttlebd.com>
2022-01-17 10:05:36 +0600

    index properties

--- advance-types/dist/app.js
+++ advance-types/dist/app.js
@@ -1,4 +1,16 @@
 "use strict";
-const userInputElement = document.getElementById('user-input');
-userInputElement.value = 'Hello World';
+let errorBag = {};
+errorBag.email = 'hello@test.com';
+errorBag.name = 'hello test';
+errorBag.age = 123;
+console.log('------------------------------');
+console.log(errorBag);
+console.log('------------------------------');
+function addition(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+const result = addition(1, 5);
 //# sourceMappingURL=app.js.map
--- advance-types/src/app.ts
+++ advance-types/src/app.ts
@@ -1,3 +1,28 @@
-const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+interface ErrorContainer {
+	[prop: string]: string | number;
+}
 
-userInputElement.value = 'Hello World';
+let errorBag: ErrorContainer = {};
+
+errorBag.email = 'hello@test.com';
+errorBag.name = 'hello test';
+errorBag.age = 123;
+
+console.log('------------------------------');
+console.log(errorBag);
+console.log('------------------------------');
+
+type numberOrString = number | string;
+type numberOrBoolean = number | boolean;
+
+type universal = numberOrString & numberOrBoolean;
+
+function addition(a: numberOrString, b: numberOrString) {
+	if (typeof a === 'string' || typeof b === 'string') {
+		return a.toString() + b.toString();
+	}
+	return a + b;
+}
+
+
+const result = addition(1, 5);
