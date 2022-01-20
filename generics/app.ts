bbb19e59d68d323e3a547ea0256c01da308e7b76
imh1j4l <hijal@shuttlebd.com>
2022-01-20 09:00:15 +0600

    generic class in ts

--- generics/app.ts
+++ generics/app.ts
@@ -1,10 +1,44 @@
-function extractAndConvert<T extends object, U extends keyof T>(
-	obj: T,
-	key: U
-) {
-	return obj[key];
+class DataStore<T extends string | number | boolean> {
+	private data: T[] = [];
+
+	addItem(item: T) {
+		this.data.push(item);
+	}
+
+	removeItem(item: T) {
+		this.data.splice(this.data.indexOf(item), 1);
+	}
+
+	getItems() {
+		return [...this.data];
+	}
 }
 
+const textStore = new DataStore<string>();
+textStore.addItem('Hello');
+textStore.addItem('World');
+textStore.removeItem('Hello');
+
+console.log(textStore.getItems());
+
+const listOfNumbers = new DataStore<number>();
+listOfNumbers.addItem(1);
+listOfNumbers.addItem(2);
+listOfNumbers.addItem(3);
+listOfNumbers.removeItem(2);
+
 console.log('------------------------------');
-console.log(extractAndConvert({ name: 'typescript' }, 'name'));
+console.log(listOfNumbers.getItems());
 console.log('------------------------------');
+
+// const storeObject = new DataStore<object>();
+
+// storeObject.addItem({ name: 'John' });
+// storeObject.addItem({ name: 'Jane' });
+// storeObject.addItem({ name: 'Joe' });
+
+// storeObject.removeItem({ name: 'Jane' });
+
+// console.log('------------------------------');
+// console.log(storeObject.getItems());
+// console.log('------------------------------');
--- generics/dist/app.js
+++ generics/dist/app.js
@@ -1,8 +1,37 @@
 "use strict";
-function extractAndConvert(obj, key) {
-    return obj[key];
+class DataStore {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
 }
+const textStore = new DataStore();
+textStore.addItem('Hello');
+textStore.addItem('World');
+textStore.removeItem('Hello');
+console.log(textStore.getItems());
+const listOfNumbers = new DataStore();
+listOfNumbers.addItem(1);
+listOfNumbers.addItem(2);
+listOfNumbers.addItem(3);
+listOfNumbers.removeItem(2);
 console.log('------------------------------');
-console.log(extractAndConvert({ name: 'typescript' }, 'name'));
+console.log(listOfNumbers.getItems());
 console.log('------------------------------');
+// const storeObject = new DataStore<object>();
+// storeObject.addItem({ name: 'John' });
+// storeObject.addItem({ name: 'Jane' });
+// storeObject.addItem({ name: 'Joe' });
+// storeObject.removeItem({ name: 'Jane' });
+// console.log('------------------------------');
+// console.log(storeObject.getItems());
+// console.log('------------------------------');
 //# sourceMappingURL=app.js.map
