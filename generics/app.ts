26d9a0cb4257d67cd788451a7425fb505b1837a9
imh1j4l <hijal@shuttlebd.com>
2022-01-20 08:45:45 +0600

    keyof in generic ts

--- generics/app.ts
+++ generics/app.ts
@@ -1,18 +1,10 @@
-interface Lengthy {
-	length: number;
-}
-
-function countAndPrint<T extends Lengthy>(element: T): [T, string] {
-	let description = 'Got no value';
-	if (element.length === 1) {
-		description = 'Got 1 element';
-	} else if (element.length > 1) {
-		description = 'Got ' + element.length + ' elements';
-	}
-	return [element, description];
+function extractAndConvert<T extends object, U extends keyof T>(
+	obj: T,
+	key: U
+) {
+	return obj[key];
 }
 
 console.log('------------------------------');
-console.log(countAndPrint(['a', 'b', 'c']));
-console.log(countAndPrint('hello typescript'));
+console.log(extractAndConvert({ name: 'typescript' }, 'name'));
 console.log('------------------------------');
--- generics/dist/app.js
+++ generics/dist/app.js
@@ -1,16 +1,8 @@
 "use strict";
-function countAndPrint(element) {
-    let description = 'Got no value';
-    if (element.length === 1) {
-        description = 'Got 1 element';
-    }
-    else if (element.length > 1) {
-        description = 'Got ' + element.length + ' elements';
-    }
-    return [element, description];
+function extractAndConvert(obj, key) {
+    return obj[key];
 }
 console.log('------------------------------');
-console.log(countAndPrint(['a', 'b', 'c']));
-console.log(countAndPrint('hello typescript'));
+console.log(extractAndConvert({ name: 'typescript' }, 'name'));
 console.log('------------------------------');
 //# sourceMappingURL=app.js.map
