74d5c0fd93f58b5ce6b1af93bc652282b1a037f2
imh1j4l <hijal@shuttlebd.com>
2022-01-23 21:05:51 +0600

    return value of decorator in ts

--- decorators/dist/app.js
+++ decorators/dist/app.js
@@ -5,58 +5,35 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
+function WithTemplate(template, hookId) {
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super(10, 10);
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent =
+                        'Area of Polygon: ' + this.area;
+                }
+            }
+        };
+    };
+}
+let Polygon = class Polygon {
+    constructor(h, w) {
+        this.height = h;
+        this.width = w;
+    }
+    get area() {
+        return this.height * this.width;
+    }
 };
-function Log(target, propertyName) {
-    console.log('Property decorator!');
-    console.log(target, propertyName);
-}
-function Log2(target, name, descriptor) {
-    console.log('Accessor decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log3(target, name, descriptor) {
-    console.log('Methods decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log4(target, name, position) {
-    console.log('parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
-}
-class Product {
-    constructor(t, p) {
-        this.title = t;
-        this._price = p;
-    }
-    set price(val) {
-        if (val > 0) {
-            this._price = val;
-        }
-        else {
-            throw new Error('Invalid price - should be positive!');
-        }
-    }
-    getPriceWithTax(tax) {
-        return this._price * (1 + tax);
-    }
-}
-__decorate([
-    Log
-], Product.prototype, "title", void 0);
-__decorate([
-    Log2
-], Product.prototype, "price", null);
-__decorate([
-    Log3,
-    __param(0, Log4)
-], Product.prototype, "getPriceWithTax", null);
-const p1 = new Product('Book', 19);
-const p2 = new Product('Book 2', 29);
+Polygon = __decorate([
+    WithTemplate('<h1>Test</h1>', 'app')
+], Polygon);
+let p1 = new Polygon(10, 20);
+console.log('------------------------------');
+console.log(p1.area);
+console.log('------------------------------');
 //# sourceMappingURL=app.js.map
--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -1,56 +1,38 @@
-function Log(target: any, propertyName: string | Symbol) {
-	console.log('Property decorator!');
-	console.log(target, propertyName);
+function WithTemplate(template: string, hookId: string) {
+	return function <T extends { new (...args: any[]): { area: number } }>(
+		originalConstructor: T
+	) {
+		return class extends originalConstructor {
+			constructor(..._: any[]) {
+				super(10, 10);
+				const hookEl = document.getElementById(hookId);
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					(hookEl.querySelector('h1') as HTMLHeadElement).textContent =
+						'Area of Polygon: ' + this.area;
+					// this.area.toString();
+				}
+			}
+		};
+	};
 }
 
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-	console.log('Accessor decorator!');
-	console.log(target);
-	console.log(name);
-	console.log(descriptor);
-}
+@WithTemplate('<h1>Test</h1>', 'app')
+class Polygon {
+	height: number;
+	width: number;
 
-function Log3(
-	target: any,
-	name: string | Symbol,
-	descriptor: PropertyDescriptor
-) {
-	console.log('Methods decorator!');
-	console.log(target);
-	console.log(name);
-	console.log(descriptor);
-}
-
-function Log4(target: any, name: string | Symbol, position: number) {
-	console.log('parameter decorator!');
-	console.log(target);
-	console.log(name);
-	console.log(position);
-}
-
-class Product {
-	@Log
-	title: string;
-	private _price: number;
-
-	@Log2
-	set price(val: number) {
-		if (val > 0) {
-			this._price = val;
-		} else {
-			throw new Error('Invalid price - should be positive!');
-		}
+	constructor(h: number, w: number) {
+		this.height = h;
+		this.width = w;
 	}
 
-	constructor(t: string, p: number) {
-		this.title = t;
-		this._price = p;
-	}
-	@Log3
-	getPriceWithTax(@Log4 tax: number) {
-		return this._price * (1 + tax);
+	get area() {
+		return this.height * this.width;
 	}
 }
 
-const p1 = new Product('Book', 19);
-const p2 = new Product('Book 2', 29);
+let p1 = new Polygon(10, 20);
+console.log('------------------------------');
+console.log(p1.area);
+console.log('------------------------------');
